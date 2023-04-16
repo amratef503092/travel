2b7888626c0c36be235ba6b1f16aea32b805fe2e
amratef503092 <41952049+amratef503092@users.noreply.github.com>
2023-04-16 02:22:58 +0200

    travel x

---
+++ app/Models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\ResetPasswordNotification;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'gender',
+        'status',
+        'nationality',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setPasswordAttribute($password)
+    {
+        return $this->attributes['password'] = Hash::make($password);
+    }
+    public function sendPasswordResetNotification($token)
+    {
+
+        $url = 'https://spa.test/reset-password?token=' . $token;
+
+        $this->notify(new ResetPasswordNotification($url));
+    }
+    public function bookings() {
+        $this->hasMany(booking::class);
+    }
+    public function hotles() {
+        $this->hasMany(hotels::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\NewPasswordController;
+
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\api\HotelsApi\LoginHotelController;
+use App\Http\Controllers\CityController;
+
+use App\Http\Controllers\Api\HotelsApi\RegisterHotelsController;
+use App\Http\Controllers\Api\EmailVerficationController;
+use App\Http\Controllers\Hotels\HotelsController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout',[LoginController::class, 'logout']);
+
+});
+// Admin Routes
+Route::post('/register',[RegisterController::class, 'register']);
+Route::post('/login',[LoginController::class, 'login']);
+Route::post('/verification-notification', [EmailVerficationController::class, 'sendVerficationEmail'])->middleware('auth:sanctum');
+Route::get('verify-email/{id}/{hash}', [EmailVerficationController::class, 'verify'])->name('verification.verify');
+Route::post('/forgot-password', [NewPasswordController::class, 'forgotPassword']);
+Route::post('/reset-password', [NewPasswordController::class, 'reset']);
+// Hotels Routes
+route::get('/Allhotles',[HotelsController::class,'index']);
+route::post('/Registerhotels',[RegisterHotelsController::class,'register']);
+route::post('/LoginHotels',[LoginHotelController::class,'login']);
+
+route::get('/City',[CityController::class,'index']);
