9aaa9caf2ecaee3dd87039b4c73b35481b6e3f71
amr atef <amr.atef503092@gmail.com>
2023-05-01 11:50:26 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['verify'=>true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
