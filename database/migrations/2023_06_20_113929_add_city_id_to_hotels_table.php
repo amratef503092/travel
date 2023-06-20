ae103bdce8117015bd2c0bfbfc99fa6df8bdc652
amratef503092 <41952049+amratef503092@users.noreply.github.com>
2023-06-20 21:38:48 +0300

    add

--- database/migrations/2023_06_20_113929_add_city_id_to_hotels_table.php
+++ database/migrations/2023_06_20_113929_add_city_id_to_hotels_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('hotels', function (Blueprint $table)
         {
-            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('city_id')->nullable();
 
             $table->foreign("city_id")->references(
                 "id")->on("cities");
